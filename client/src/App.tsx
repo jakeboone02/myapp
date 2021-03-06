31a82144f95d31c3082708562060adc0eac30711
Jake Boone <jakeboone02@gmail.com>
2021-03-06 11:58:10 -0700

    M02L01

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,9 @@ import QueryBuilder, { formatQuery, RuleGroupType } from "react-querybuilder";
 import { useState } from 'react';
 import fields from './fields';
 import getOperators from './getOperators';
+import { Language } from './types';
+import translations from './translations';
+import combinators from './combinators';
 
 function App() {
   const [query, setQuery] = useState<RuleGroupType>({
@@ -10,14 +13,21 @@ function App() {
     combinator: 'and',
     rules: [],
   });
+  const [language, setLanguage] = useState<Language>("en");
 
   return (
     <>
+      <select value={language} onChange={e => setLanguage(e.target.value as Language)}>
+        <option value="en">English</option>
+        <option value="es">Spanish</option>
+      </select>
       <QueryBuilder
         fields={fields}
         onQueryChange={q => setQuery(q)}
         query={query}
         getOperators={getOperators}
+        translations={translations[language]}
+        combinators={combinators[language]}
       />
       <pre>{formatQuery(query, 'sql')}</pre>
       <pre>{formatQuery(query, 'json')}</pre>
---
+++ client/src/combinators.ts
@@ -0,0 +1,12 @@
+import { defaultCombinators, NameLabelPair } from "react-querybuilder";
+import { Language } from "./types";
+
+const combinators: Record<Language, NameLabelPair[]> =  {
+  en: defaultCombinators,
+  es: [
+    { name: "and", label: "Y" },
+    { name: "or", label: "O" },
+  ]
+};
+
+export default combinators;
