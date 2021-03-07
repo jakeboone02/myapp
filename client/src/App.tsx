0cb189fad6e81029af3f1da24c83133880d2128d
Jake Boone <jakeboone02@gmail.com>
2021-03-07 15:20:56 -0700

    M03L01

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,35 @@
-import { useState } from 'react';
+import { useState } from "react";
 import QueryBuilder, { formatQuery, RuleGroupType } from "react-querybuilder";
-import './App.scss';
-import combinators from './combinators';
-import CombinatorSelector from './CombinatorSelector';
-import fields from './fields';
-import getOperators from './getOperators';
-import translations from './translations';
-import { Language } from './types';
-import ValueEditor from './ValueEditor';
+import "./App.scss";
+import combinators from "./combinators";
+import CombinatorSelector from "./CombinatorSelector";
+import fields from "./fields";
+import getOperators from "./getOperators";
+import translations from "./translations";
+import { Language } from "./types";
+import ValueEditor from "./ValueEditor";
+import valueProcessor from "./valueProcessor";
 
 function App() {
   const [query, setQuery] = useState<RuleGroupType>({
-    id: 'root',
-    combinator: 'and',
+    id: "root",
+    combinator: "and",
     rules: [],
   });
   const [language, setLanguage] = useState<Language>("en");
 
   return (
     <>
-      <select value={language} onChange={e => setLanguage(e.target.value as Language)}>
+      <select
+        value={language}
+        onChange={(e) => setLanguage(e.target.value as Language)}
+      >
         <option value="en">English</option>
         <option value="es">Spanish</option>
       </select>
       <QueryBuilder
         fields={fields}
-        onQueryChange={q => setQuery(q)}
+        onQueryChange={(q) => setQuery(q)}
         query={query}
         getOperators={getOperators}
         translations={translations[language]}
@@ -35,8 +39,8 @@ function App() {
           valueEditor: ValueEditor,
         }}
       />
-      <pre>{formatQuery(query, 'sql')}</pre>
-      <pre>{formatQuery(query, 'json')}</pre>
+      <pre>{formatQuery(query, { format: "sql", valueProcessor })}</pre>
+      <pre>{formatQuery(query, "json")}</pre>
     </>
   );
 }
