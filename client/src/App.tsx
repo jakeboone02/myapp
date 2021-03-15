b1c033e83279216e7b8db7e317b078ad92fbd999
Jake Boone <jakeboone02@gmail.com>
2021-03-15 15:26:08 -0700

    M06L02-3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
-import { Button, MenuItem, Select } from "@material-ui/core";
+import { Button, Grid, MenuItem, Select, Typography } from "@material-ui/core";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import { AgChartsReact } from "ag-charts-react";
 import { ColDef, GridApi } from "ag-grid-community";
 import "ag-grid-community/dist/styles/ag-grid.css";
-import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import "ag-grid-community/dist/styles/ag-theme-material.css";
 import { AgGridReact } from "ag-grid-react";
 import { addDays, format, parseISO } from "date-fns";
 import { useState } from "react";
@@ -154,146 +154,160 @@ function App() {
   };
 
   return (
-    <>
-      <Select
-        value={language}
-        onChange={(e) => setLanguage(e.target.value as Language)}
-      >
-        <MenuItem value="en">English</MenuItem>
-        <MenuItem value="es">Spanish</MenuItem>
-      </Select>
-      <Select
-        value={dataset}
-        onChange={(e) => setDataset(e.target.value as Dataset)}
-      >
-        <MenuItem value="sales">Sales</MenuItem>
-        <MenuItem value="unlocode">UN/LOCODE</MenuItem>
-      </Select>
-      <QueryBuilder
-        fields={fields[dataset]}
-        onQueryChange={(q) => (dataset === "sales" ? setQuery : setQueryUNL)(q)}
-        query={dataset === "sales" ? query : queryUNL}
-        getOperators={getOperators}
-        translations={translations[language]}
-        combinators={combinators[language]}
-        controlElements={{
-          addGroupAction: MaterialActionElement,
-          addRuleAction: MaterialActionElement,
-          fieldSelector: MaterialValueSelector,
-          notToggle: MaterialNotToggle,
-          operatorSelector: MaterialValueSelector,
-          removeGroupAction: MaterialActionElement,
-          removeRuleAction: MaterialActionElement,
-          combinatorSelector: MaterialValueSelector,
-          valueEditor: ValueEditor,
-        }}
-      />
-      <Button onClick={dataset === "sales" ? getData : getDataUNL}>
-        Get Data
-      </Button>
-      <QueryBuilder
-        fields={fields[dataset]}
-        onQueryChange={(q) => setUpdateQuery(q)}
-        query={updateQuery}
-        getOperators={getOperatorsForUpdate}
-        controlElements={{
-          addGroupAction: () => null,
-          combinatorSelector: () => null,
-          addRuleAction: MaterialActionElement,
-          fieldSelector: MaterialValueSelector,
-          notToggle: MaterialNotToggle,
-          operatorSelector: MaterialValueSelector,
-          removeGroupAction: MaterialActionElement,
-          removeRuleAction: MaterialActionElement,
-          valueEditor: ValueEditorForBulkEdit,
-        }}
-      />
-      <Button type="button" onClick={onClickUpdate}>
-        Update
-      </Button>
-      <div className="ag-theme-alpine" style={{ height: 400, width: "100%" }}>
-        <AgGridReact
-          columnDefs={[
-            {
-              field: "selectionColumn",
-              headerCheckboxSelection: true,
-              checkboxSelection: true,
-              width: 40,
-            },
-            ...(dataset === "sales" ? columnDefs : columnDefsUNL),
-          ]}
-          onGridReady={(gre) => setGridApi(gre.api)}
-          rowData={dataset === "sales" ? rawData : rawDataUNL}
-          rowSelection="multiple"
-          suppressPropertyNamesCheck
-        />
-      </div>
-      {dataset === "sales" ? (
-        <AgChartsReact
-          options={{
-            data: processChartData(chartData),
-            series: [
-              {
-                type: "line",
-                xKey: "order_month",
-                yKey: "revenue",
-              },
-              {
-                type: "line",
-                xKey: "order_month",
-                yKey: "profit",
-              },
-            ],
-            axes: [
-              {
-                type: "time",
-                position: "bottom",
-              },
-              {
-                type: "number",
-                position: "left",
-              },
-            ],
+    <Grid container spacing={2}>
+      <Grid item xs={2}>
+        <Typography variant="h6" gutterBottom>
+          Language
+        </Typography>
+        <Select
+          value={language}
+          onChange={(e) => setLanguage(e.target.value as Language)}
+        >
+          <MenuItem value="en">English</MenuItem>
+          <MenuItem value="es">Spanish</MenuItem>
+        </Select>
+        <Typography variant="h6" gutterBottom>
+          Dataset
+        </Typography>
+        <Select
+          value={dataset}
+          onChange={(e) => setDataset(e.target.value as Dataset)}
+        >
+          <MenuItem value="sales">Sales</MenuItem>
+          <MenuItem value="unlocode">UN/LOCODE</MenuItem>
+        </Select>
+      </Grid>
+      <Grid item xs={10}>
+        <QueryBuilder
+          fields={fields[dataset]}
+          onQueryChange={(q) =>
+            (dataset === "sales" ? setQuery : setQueryUNL)(q)
+          }
+          query={dataset === "sales" ? query : queryUNL}
+          getOperators={getOperators}
+          translations={translations[language]}
+          combinators={combinators[language]}
+          controlElements={{
+            addGroupAction: MaterialActionElement,
+            addRuleAction: MaterialActionElement,
+            fieldSelector: MaterialValueSelector,
+            notToggle: MaterialNotToggle,
+            operatorSelector: MaterialValueSelector,
+            removeGroupAction: MaterialActionElement,
+            removeRuleAction: MaterialActionElement,
+            combinatorSelector: MaterialValueSelector,
+            valueEditor: ValueEditor,
           }}
         />
-      ) : rawDataUNL?.length ? (
-        <LoadScript
-          googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY!}
-          language={language}
-        >
-          <GoogleMap
-            mapContainerStyle={{ width: "100%", height: "600px" }}
-            center={{ lat: 0, lng: 0 }}
-            zoom={2}
-          >
-            {rawDataUNL
-              .filter((location) => location.latitude && location.longitude)
-              .map((location) => (
-                <Marker
-                  key={location.id}
-                  position={{
-                    lat: location.latitude,
-                    lng: location.longitude,
-                  }}
-                  title={location.name}
-                />
-              ))}
-          </GoogleMap>
-        </LoadScript>
-      ) : (
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            height: 600,
-            width: "100%",
+        <Button onClick={dataset === "sales" ? getData : getDataUNL}>
+          Get Data
+        </Button>
+        <QueryBuilder
+          fields={fields[dataset]}
+          onQueryChange={(q) => setUpdateQuery(q)}
+          query={updateQuery}
+          getOperators={getOperatorsForUpdate}
+          controlElements={{
+            addGroupAction: () => null,
+            combinatorSelector: () => null,
+            addRuleAction: MaterialActionElement,
+            fieldSelector: MaterialValueSelector,
+            notToggle: MaterialNotToggle,
+            operatorSelector: MaterialValueSelector,
+            removeGroupAction: MaterialActionElement,
+            removeRuleAction: MaterialActionElement,
+            valueEditor: ValueEditorForBulkEdit,
           }}
-        >
-          Load data to see map
+        />
+        <Button type="button" onClick={onClickUpdate}>
+          Update
+        </Button>
+        <div className="ag-theme-material" style={{ height: 400, width: "100%" }}>
+          <AgGridReact
+            columnDefs={[
+              {
+                field: "selectionColumn",
+                headerCheckboxSelection: true,
+                checkboxSelection: true,
+                width: 40,
+              },
+              ...(dataset === "sales" ? columnDefs : columnDefsUNL),
+            ]}
+            onGridReady={(gre) => setGridApi(gre.api)}
+            rowData={dataset === "sales" ? rawData : rawDataUNL}
+            rowSelection="multiple"
+            suppressPropertyNamesCheck
+          />
         </div>
-      )}
-    </>
+        {dataset === "sales" ? (
+          <div style={{ height: 600 }}>
+            <AgChartsReact
+              options={{
+                data: processChartData(chartData),
+                series: [
+                  {
+                    type: "line",
+                    xKey: "order_month",
+                    yKey: "revenue",
+                  },
+                  {
+                    type: "line",
+                    xKey: "order_month",
+                    yKey: "profit",
+                  },
+                ],
+                axes: [
+                  {
+                    type: "time",
+                    position: "bottom",
+                  },
+                  {
+                    type: "number",
+                    position: "left",
+                  },
+                ],
+              }}
+            />
+          </div>
+        ) : rawDataUNL?.length ? (
+          <LoadScript
+            googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY!}
+            language={language}
+          >
+            <GoogleMap
+              mapContainerStyle={{ width: "100%", height: "600px" }}
+              center={{ lat: 0, lng: 0 }}
+              zoom={2}
+            >
+              {rawDataUNL
+                .filter((location) => location.latitude && location.longitude)
+                .map((location) => (
+                  <Marker
+                    key={location.id}
+                    position={{
+                      lat: location.latitude,
+                      lng: location.longitude,
+                    }}
+                    title={location.name}
+                  />
+                ))}
+            </GoogleMap>
+          </LoadScript>
+        ) : (
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              height: 600,
+              width: "100%",
+            }}
+          >
+            Load data to see map
+          </div>
+        )}
+      </Grid>
+    </Grid>
   );
 }
 
